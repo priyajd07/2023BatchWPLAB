091450651d4492089eed2d2d49571797202ab936
muhd-shafeen-34 <145667235+muhd-shafeen-34@users.noreply.github.com>
2023-12-07 12:36:56 +0530

    Add files via upload

---
+++ 34_MUHAMMED_SHAFEEN_VK/program_8/script.js
@@ -0,0 +1,11 @@
+const p = getElementById("p1");
+		function change1(img)
+		{
+			img.src = "image2.jpg";
+			document.getElementById("p1").innerHTML="rose garden";
+    	}
+		function change2(img)
+		{
+			img.src = "image1.jpg";
+			document.getElementById("p1").innerHTML="tulip garden";		
+    	}
