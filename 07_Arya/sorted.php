14c3e6a89d69d709c8f3a78302fbbcdc714cc202
aryaanadam55 <145668673+aryaanadam55@users.noreply.github.com>
2023-12-12 16:24:48 +0530

    Add files via upload

---
+++ 07_Arya/sorted.php
@@ -0,0 +1,13 @@
+<?php
+$students=array("pra"=>"pranav","rin"=>"rinu","rah"=>"rahul","adi"=>"aditya","fid"=>"fida");
+echo"<h1>student name sorting</h1>";
+print_r($students);
+echo"<br>";
+echo"array sored using asort:<br>";
+asort($students);
+print_r($students);
+echo"<br>";
+echo"array soring using asort:<br>";
+asort($students);
+print_r($students);
+?>
