bf5ca6a5c8b79b18ef5d906e150c7d41aad57d1b
Nabeel-jr <145667291+Nabeel-jr@users.noreply.github.com>
2023-12-12 16:27:58 +0530

    Create student.php

---
+++ 31_Nabeel/program 10/student.php
@@ -0,0 +1,11 @@
+<?php
+$s=array(1=>"Nabeel",2=>"Amir",3=>"Jaseel",9=>"Faheem",5=>"Shaheel");
+echo"<br><br>THE ARRAY ELEMENTS:<BR>";
+print_r($s);
+asort($s);
+echo"<br><br>THE ARRAY ELEMENTS AFTER SORTED:<BR>";
+print_r($s);
+arsort($s);
+echo"<br><br>THE ARRAY ELEMENTS AFTER REVERSE SORTED:<BR>";
+print_r($s);
+?>
