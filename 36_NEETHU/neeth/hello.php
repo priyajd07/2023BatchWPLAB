f5217523e17572dc7e76ecd9113671d6cd0fffc3
neethupdas <146073373+neethupdas@users.noreply.github.com>
2023-12-13 16:29:08 +0530

    Add files via upload

---
+++ 36_NEETHU/neeth/hello.php
@@ -0,0 +1,18 @@
+<?php
+$x=10;
+echo "$x ";
+echo "<br>";
+print "welcome ";
+echo "<br>";
+/*associative array:*/
+$age = array("Peter"=>"40", "Ben"=>"17", "Joe"=>"63");
+echo "Peter is " . $age['Peter'] . " years old.";
+/*sort*/
+sort($age);
+print_r($age);
+echo "<br>";
+$a = array("red", "green","blue","yellow");
+print_r($a);
+echo "<br>";
+echo "hello";
+?>
