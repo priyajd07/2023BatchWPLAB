2584c9a9455758c68fa64713ca4ab24fcef1365c
akshaya2248 <146075091+akshaya2248@users.noreply.github.com>
2023-12-13 16:34:22 +0530

    Add files via upload

---
+++ 02_akshaya/variable.php
@@ -0,0 +1,17 @@
+<?php
+$txt1 = "Learn PHP";
+$txt2 = "Schools.com";
+$x = 5;
+$y = 4;
+
+echo "<h2>" . $txt1 . "</h2>";
+echo "Study PHP at " . $txt2 . "<br>";
+echo $x + $y;
+echo "<br>";
+print_r($x);
+echo "<br>";
+
+$names = array("preetha","divya","kiranan","akshaya");
+sort($names);
+print_r($names);
+?>
