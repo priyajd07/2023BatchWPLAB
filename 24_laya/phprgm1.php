8a8b9300b322c1a1ebd8b31bd7b7fdb8b6bb2b61
layasimon <146072706+layasimon@users.noreply.github.com>
2023-12-13 16:28:49 +0530

    Add files via upload

---
+++ 24_laya/phprgm1.php
@@ -0,0 +1,15 @@
+
+
+<?php
+$students = array("pra"=>"pranav","rin"=>"rinu","rah"=>"rahul","adi"=>"adithya","fid"=>"fida");
+echo "<h1> Student Name Sorting </h1>";
+print_r($students);
+echo"<br/>";
+echo "Array Sorted Using asort : <br/>";
+asort($students);
+print_r($students);
+echo "<br/>";
+echo "Array Sorting using arsort: <br/>";
+arsort($students);
+print_r($students);
+?>
---
+++ 24_laya/phprgm2.php
@@ -0,0 +1,30 @@
+
+
+
+<html>
+<head> 
+<style>
+.C
+{
+border: solid 1px; font-size:28px; padding:3;
+}
+</style> 
+</head>
+ <body>
+ <?php
+$data = array(array('1', 'Sachin', 'Tendulkar'), array('2', 'Virat', 'Kohli' ),array('3','MS', 'Dhoni'));
+echo "<h1>Indian Cricket Players</h1>";
+echo "<table class = c>";
+echo "<tr class = c><th class = c>No.</th><th class =c>First Name</th><th class = c>Last Name</th></tr>";
+foreach($data as $a)
+{ echo "<tr class = c>";
+foreach($a as $v)
+{
+echo "<td class = c>".$v."</td>";
+}
+echo "</tr>";
+}
+echo "</table>";
+?>
+</body> 
+</html>
