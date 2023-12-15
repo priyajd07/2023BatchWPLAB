942efc4528ff3029e64c34347d640b82aa8fa1be
shamna53 <146523370+shamna53@users.noreply.github.com>
2023-12-15 16:02:55 +0530

    Update student.php

--- 53_shamna_sherin/program10/student.php
+++ 53_shamna_sherin/program10/student.php
@@ -4,11 +4,11 @@ $student=array("Safa"=>"safu", "Shamla"=>"shalu", "Sreeshna"=>"shabu", "Gopika"=
 echo "<h1>Students name sort</h1>";
 print_r($student);
 echo "<br>";
-echo "array sorted using asort:<br>";
+echo "<h3>array sorted using asort:<br></h3>";
 asort($student);
 print_r($student);
 echo "<br>";
-echo "array sorted using arsort:<br>";
+echo "<h3>array sorted using arsort:<br></h3>";
 asort($student);
 print_r($student);
 echo "<br>";
