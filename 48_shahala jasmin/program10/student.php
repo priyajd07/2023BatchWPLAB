0219f7bdd88eed190f21e66df0a54f2a5e0e4fa6
ShahalaJasminn <146523851+ShahalaJasminn@users.noreply.github.com>
2023-12-15 16:28:00 +0530

    Update student.php

--- 48_shahala jasmin/program10/student.php
+++ 48_shahala jasmin/program10/student.php
@@ -1,13 +1,14 @@
 <?php
 $student=array("pra"=>"pranav","sha"=>"shahala","nith"=>"nitha");
 echo "<h1>Student Name Sorting</h1>";
+echo"<h2>Main Array</h2>";
 print_r($student);
-echo "<br>";
-echo "Array sorted using asort:<br>";
+echo "<br> <br>";
+echo "<h2>Array sorted using asort:<br></h2>";
 asort($student);
 print_r($student);
-echo"<br>";
-echo "Array sorting using arsort:<br>";
+echo"<br> <br>";
+echo "<h2>Array sorting using arsort:<br></h2>";
 arsort($student);
 print_r($student);
 ?>
