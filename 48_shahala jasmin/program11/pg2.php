de092f8339dd3c2dca86169fb5846a378a341a8d
ShahalaJasminn <146523851+ShahalaJasminn@users.noreply.github.com>
2023-12-15 16:24:44 +0530

    Update pg2.php

--- 48_shahala jasmin/program11/pg2.php
+++ 48_shahala jasmin/program11/pg2.php
@@ -3,15 +3,15 @@
 <style>
 .c
 {
-border:solid 1px;font_size:28px;padding:3;
+border:solid 3px;font_size:48 px; padding:13;
 }
 </style>
 </head>
 <body>
 <?php
-$data=array(array('1','sachin','tendulkar'),array('2','virat','kohli'),array('3','MS','dhoni'));
+$data=array(array('1','sachin','tendulkar'),array('2','virat','kohli'),array('3','MS','dhoni'),array('4','MS','dhoni'));
 echo "<h1>Indian Cricket Players</h1>";
-echo "<table class=c>";
+echo "<table  class=c>";
 echo "<tr class=c><th class=c>No.</th><th class=c>First Name</th><th class=c>Last Name</th></tr>";
 foreach($data as $a)
 {
