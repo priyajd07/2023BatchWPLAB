e81831cd4c44031a6e15388759dc95bf092fba50
ShahalaJasminn <146523851+ShahalaJasminn@users.noreply.github.com>
2023-12-15 15:53:01 +0530

    Update bill.php

--- 48_shahala jasmin/program12/bill.php
+++ 48_shahala jasmin/program12/bill.php
@@ -9,9 +9,9 @@
 </form>
 </body>
 <?php
-$result_str=$result=";
+$result_str=$result="";
 if(isset($_POST['unit-submit'])){
-$units=$_POST['units];
+$units=$_POST['units'];
 if(!empty($units)){
 	$result=calculate_bill($units);
 	$result_str='<h2>Total amount of'.$units.'-'.$result."</h2>";
@@ -23,7 +23,7 @@ function calculate_bill($units){
 	$unit_cost_second=4.00;
 	$unit_cost_third=5.20;
 	$unit_cost_fourth=6.50;
-	if($unit<=50){
+	if($units<=50){
 	$bill=$units*$unit_cost_first;
 	}
 	else if($units>50&&$units<=100){
@@ -34,7 +34,7 @@ function calculate_bill($units){
 
 
 
-else if($units > 50 && $unit <=100)
+else if($units > 50 && $units <=100)
 {
 $temp=50* $unit_cost_first;
 $remaining_units=$units-50;
