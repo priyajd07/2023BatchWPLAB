a537d13349643715e48d2ca9898acb18461e19d1
shamlakv <146522964+shamlakv@users.noreply.github.com>
2023-12-15 16:00:24 +0530

    Update program10.php

--- 52_shamla/program10.php
+++ 52_shamla/program10.php
@@ -5,11 +5,11 @@ $student=array("shalu"=>"shamla", "safu"=>"safa","sree"=> "sreeshna","gopi"=>"go
 echo "<h1>Student Name Sorting</h1>";
 print_r($student);
 echo "<br>";
-echo "Array Sorted Using asort:<br>";
+echo "<h2>Array Sorted Using asort:</h2><br>";
 asort($student);
 print_r($student);
 echo "<br/>";
-echo "Array Sorted Using arsort:<br>";
+echo "<h2>Array Sorted Using arsort:</h2><br>";
 arsort($student);
 print_r($student);
 echo "<br>";
