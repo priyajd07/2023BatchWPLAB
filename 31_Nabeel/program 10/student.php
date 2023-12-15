a08c1ff505a55815821046c310512f18f0c7ea52
Nabeel-jr <145667291+Nabeel-jr@users.noreply.github.com>
2023-12-15 16:23:22 +0530

    Update student.php

--- 31_Nabeel/program 10/student.php
+++ 31_Nabeel/program 10/student.php
@@ -1,6 +1,7 @@
 <?php
+echo"<h1><u>STUDENT NAME SORTING</u></h1><h3>";
 $s=array(1=>"Nabeel",2=>"Amir",3=>"Jaseel",9=>"Faheem",5=>"Shaheel");
-echo"<br><br>THE ARRAY ELEMENTS:<BR>";
+echo"THE ARRAY ELEMENTS:<BR>";
 print_r($s);
 asort($s);
 echo"<br><br>THE ARRAY ELEMENTS AFTER SORTED:<BR>";
