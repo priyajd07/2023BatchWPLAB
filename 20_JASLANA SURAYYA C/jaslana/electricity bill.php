3224615375c97dab125468792ccd827615b7b601
jaslanasurayyac <146112544+jaslanasurayyac@users.noreply.github.com>
2023-12-15 15:50:11 +0530

    Add files via upload

--- 20_JASLANA SURAYYA C/jaslana/electricity bill.php
+++ 20_JASLANA SURAYYA C/jaslana/electricity bill.php
@@ -5,7 +5,7 @@ input
 {
 border:solid 2px;
 padding:5px;
-fontsize:28px;
+font-size:28px;
 }
 </style>
 </head>
@@ -42,12 +42,18 @@ else if($units>50&&units<=100)
 {
 $temp=50*$unit_cost_first;
 $remaining_units=$units-50;
+$bill=$temp+($remaining_units*$unit_cost_second);
+}
+else if($units>100&&$units<=200)
+{
+$temp=(50*3.5)+(100*$unit_cost_second);
+$remaining_units=$unit-150;
 $bill=$temp+($remaining_units*$unit_cost_third);
 }
 else
 {
-$temp=(50*3.5)+(100*$unit_cost_second)+(100*$unit_cost_third);
-$remaining_units=$unit-250;
+$temp=(50*3.5)+(100*$units_cost_second)+(100*$unit_cost_third);
+$remaining_units=$units-250;
 $bill=$temp+($remaining_units*$unit_cost_fourth);
 }
 return number_format((float)$bill, 2, '.', '');
