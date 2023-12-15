f78f870eb0fb79432cfe9a9e0d7644dab66bbd78
sreeshna2000 <113668836+sreeshna2000@users.noreply.github.com>
2023-12-15 15:45:57 +0530

    Add files via upload

---
+++ 54_Sreeshna K S/prgm10.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<?php
+$students = array("minnu"=>"Sreeshna","ponnu"=>"Gopika", "unnimol"=>"Shabana","unnimol"=>"Shamla","safu"=>"Safa");
+echo "<h1>students name sorting</h1>";
+print_r($students);
+echo"<br>";
+echo"array sorted using asort:<br>";
+asort($students);
+print_r($students);
+echo"<br>";
+echo"array sorted using arsort:<br>";
+arsort($students);
+print_r($students);
+
+?>
