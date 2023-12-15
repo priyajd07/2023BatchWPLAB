6de3b5c066edcf0b0de5ce2a591be4ca6ec24dac
jaslanasurayyac <146112544+jaslanasurayyac@users.noreply.github.com>
2023-12-15 16:09:15 +0530

    Add files via upload

--- 20_JASLANA SURAYYA C/jaslana/student.php
+++ 20_JASLANA SURAYYA C/jaslana/student.php
@@ -1,12 +1,18 @@
+<html>
+<body>
 <?php
-$students=array("eg"=>"eugine","ye"=>"yesol","is"=>"ishan");
-echo "<h1>Student Name Sorting</h1>";
-print_r($students);
-echo "<br/>";
-echo "Array Sorted Using asort:<br/>";
-asort($students);
-print_r($students);
-echo "<br/>";
-echo "Array Sorted Using asort:<br/>";
-asort($students);
-print_r($students);
+$student=array("pi"=>"Pinky","ri"=>"Rinu","Anu"=>"Aneesha","anj"=>"Anjana","amm"=>"Ammu");
+echo "<h1>Students name sorting</h1>";
+print_r($student);
+echo "<br>";
+echo"<br>";
+echo "<b>Array sorted using asort </b><br/>";
+asort($student);
+echo"<br>";
+print_r($student);
+echo "<br>";
+echo"<br>";
+echo "<b>Array sorted using arsort </b><br/>";
+echo"<br>";
+arsort($student);
+print_r($student);
