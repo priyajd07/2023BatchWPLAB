881a4747e9028be498356fa8cf9aa5bbf996f4cc
aryaanadam55 <145668673+aryaanadam55@users.noreply.github.com>
2023-12-15 15:43:45 +0530

    Add files via upload

--- 07_Arya/program12.php
+++ 07_Arya/program12.php
@@ -38,10 +38,16 @@ if($units<=50)
 {
 $bill=$units*$unit_cost_first;
 }
-else if($units>50&&units<=100)
+else if($units>50&&$units<=100)
 {
 $temp=50*$unit_cost_first;
 $remaining_units=$units-50;
+$bill=$temp+($remaining_units*$unit_cost_third);
+}
+else if($units>100&&units<=200)
+{
+$temp=(50*3.5)+(100*$unit_cost_second);
+$remaining_units=$units=150;
 $bill=$temp+($remaining_units*$unit_cost_third);
 }
 else
